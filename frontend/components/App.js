6102a3f2be543051e74c763c0c56f4b883166923
Roberto Andino <andinorobert518@yahoo.com>
2024-08-27 13:47:15 -0400

    Done

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import axios from "axios"
+import Form from './Form'
+import TodoList from './TodoList'
 
 const URL = 'http://localhost:9000/api/todos'
 
@@ -12,7 +14,33 @@ export default class App extends React.Component {
     this.state = {
       todos: [],
       error: '',
+      todoNameInput: '',
+      displayCompleteds: true,
     }
+  }
+
+  onChange = evt => {
+    const { value } = evt.target //extracts value typed from evt
+    this.setState({ ...this.state, todoNameInput: value }) //Updating todoNameInput state to value
+  }
+
+  resetForm = () => this.setState({ ...this.state, todoNameInput: '' })
+
+  setAxiosResponseError = err => this.setState({ ...this.state, error: err.response.data.message })
+
+  postNewTodo = () => {
+    axios.post(URL, { name: this.state.todoNameInput}) //The raw data posted needs to be an object name
+    .then(res => {
+      //this.fetchTodos()
+      this.setState({ ...this.state, todos: this.state.todos.concat(res.data.data) })
+      this.resetForm() //resets the form after successful submit
+    })
+    .catch(this.setAxiosResponseError)
+  }
+
+  onSubmit = evt => {
+    evt.preventDefault() //prevent page refreshing when hitting submit
+    this.postNewTodo()
   }
 
   fetchTodos = () => {
@@ -20,9 +48,24 @@ export default class App extends React.Component {
       .then(res => {
         this.setState({ ...this.state, todos: res.data.data }) //...this.state destructures the states but we only change todos
       })
-      .catch(err => {
-        this.setState({ ...this.state, error: err.response.data.message }) //Here we only update the state of error
+      .catch(this.setAxiosResponseError)
+  }
+
+  toggleCompleted = id => () => {
+    axios.patch(`${URL}/${id}`)
+      .then(res =>{
+        this.setState({
+          ...this.state, todos: this.state.todos.map(td => {
+            if (td.id !== id) return td  
+            return res.data.data
+          })
+        })
       })
+      .catch(this.setAxiosResponseError)
+  }
+
+  toggleDisplayCompleteds = () =>{
+    this.setState({ ...this.state, displayCompleteds: !this.state.displayCompleteds })
   }
 
   componentDidMount() {
@@ -34,19 +77,18 @@ export default class App extends React.Component {
     return (
       <div>
         <div id="error">Error: { this.state.error }</div>
-        <div id="todos">
-          <h2>Todos:</h2>
-          {
-            this.state.todos.map(td => {
-              return <div key={td.id}>{td.name}</div>
-            })
-          }
-        </div>
-        <form id="todoForm">
-          <input type="text" placeholder="Type todo"></input>
-          <input type="submit"></input>
-          <button>Clear Completed</button>
-        </form>
+        <TodoList 
+          todos={this.state.todos}
+          displayCompleteds={this.state.displayCompleteds}
+          toggleCompleted={this.toggleCompleted}   
+        />
+        <Form 
+          onSubmit={this.onSubmit}
+          todoNameInput={this.state.todoNameInput}
+          onChange={this.onChange}
+          toggleDisplayCompleteds={this.toggleDisplayCompleteds}
+          displayCompleteds={this.state.displayCompleteds}
+        />
       </div>
     )
   }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -2,6 +2,24 @@ import React from 'react'
 
 export default class Form extends React.Component {
   render() {
-    return null
+
+    return(
+      <>
+        <form id="todoForm" onSubmit={this.props.onSubmit}>
+          <input 
+            value={this.props.todoNameInput } 
+            onChange={this.props.onChange} 
+            type="text" 
+            placeholder="Type todo">
+          </input>
+          <input type="submit"></input>
+        </form>
+        <button 
+          onClick={this.props.toggleDisplayCompleteds}
+        >
+          {this.props.displayCompleteds ? 'Hide' : 'Show'} Completed
+        </button>
+      </>
+    )
   }
 }
